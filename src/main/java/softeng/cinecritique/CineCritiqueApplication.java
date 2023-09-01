2f68f5bcfcfeda8a1a72f39b9756f123d6d6cc41
Pe-Guedss <pedro.og2002@gmail.com>
2023-09-01 18:36:33 -0300

    Feat: Initializing the repository.

---
+++ src/main/java/softeng/cinecritique/CineCritiqueApplication.java
@@ -0,0 +1,13 @@
+package softeng.cinecritique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineCritiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineCritiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/softeng/cinecritique/CineCritiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package softeng.cinecritique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineCritiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
